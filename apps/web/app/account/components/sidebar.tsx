1389be3f37bb073654873a14c61fdc01a26f8a0b
Akash Shaw <shawakash2003@gmail.com>
2024-03-31 21:47:55 +0530

    chore: hover and select effect on sidenav bar

--- apps/web/app/account/components/sidebar.tsx
+++ apps/web/app/account/components/sidebar.tsx
@@ -36,7 +36,7 @@ export function Sidenav({ links, isCollapsed, selectedTab, setSelectedTab }: Nav
             data-collapsed={isCollapsed}
             className="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2"
         >
-            <nav className="grid gap-1.5 px-4 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
+            <nav className="grid gap-2 px-4 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
                 {links.map((link, index) =>
                     isCollapsed ? (
                         <Tooltip key={index} delayDuration={0}>
@@ -46,16 +46,19 @@ export function Sidenav({ links, isCollapsed, selectedTab, setSelectedTab }: Nav
                                     onClick={() => setSelectedTab(link.id)}
                                     className={cn(
                                         buttonVariants({ variant: link.variant, size: "icon" }),
-                                        "h-10 w-10",
+                                        "h-10 w-10 rounded-full  dark:text-muted-foreground ",
+                                        link.id !== selectedTab && "dark:hover:text-slate-100",
                                         link.id === selectedTab &&
-                                        "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white "
+                                        "dark:bg-white dark:text-black dark:hover:font-bold"
                                     )}
                                 >
-                                    <link.icon className="h-5 w-5" />
+                                    <link.icon className={cn(
+                                        "h-5 w-5",
+                                    )} />
                                     <span className="sr-only">{link.title}</span>
                                 </Link>
                             </TooltipTrigger>
-                            <TooltipContent side="right" className="flex items-center gap-4">
+                            <TooltipContent side="right" className="flex items-center gap-4 relative -right-2">
                                 {link.title}
                                 {link.label && (
                                     <span className="ml-auto text-muted-foreground">
