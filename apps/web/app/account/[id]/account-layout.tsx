32e49f68815686ef9ad0f1515926c5bcec2be42d
Akash Shaw <shawakash2003@gmail.com>
2024-03-31 21:48:49 +0530

    chore: added / page for account showing public address

--- apps/web/app/account/[id]/account-layout.tsx
+++ apps/web/app/account/[id]/account-layout.tsx
@@ -1,8 +1,17 @@
 "use client"
 
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
 import { AccountType } from "@paybox/common"
 import { accountAtom, accountsAtom } from "@paybox/recoil"
-import { useRouter } from "next/navigation"
+import Link from "next/link"
+import { usePathname, useRouter } from "next/navigation"
 import { useEffect } from "react"
 import { useRecoilValue, useSetRecoilState } from "recoil"
 import { toast } from "sonner"
@@ -19,13 +28,14 @@ export const AccountLayout = ({
     const setAccount = useSetRecoilState(accountAtom);
     const accounts = useRecoilValue(accountsAtom);
     const router = useRouter();
-    
+    const path = usePathname();
+
     useEffect(() => {
         if (account) {
             setAccount(account)
         } else {
             const acc = accounts.find(acc => acc.id === id);
-            if(!acc) {
+            if (!acc) {
                 toast.error("Account not found");
                 router.push('/account/');
                 return;
@@ -38,7 +48,30 @@ export const AccountLayout = ({
 
     return (
         <>
-            {children}
+            <div className="h-screen p-4">
+                <div className="">
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem key={"home"}>
+                                <BreadcrumbLink href={`/`}>Home</BreadcrumbLink>
+                            </BreadcrumbItem>
+                            {path.split("/").map((item, index) => {
+                                let link = path.split("/").slice(0, index + 1).join("/")
+                                return (
+                                    <>
+                                        <BreadcrumbItem key={index}>
+                                            <BreadcrumbLink href={link}>{item}</BreadcrumbLink>
+                                        </BreadcrumbItem>
+                                        <BreadcrumbSeparator />
+                                    </>
+                                )
+                            })}
+
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </div>
+                {children}
+            </div>
         </>
     );
 }
---
+++ apps/web/app/account/[id]/tab.tsx
@@ -0,0 +1,148 @@
+"use client"
+import EthIcon from '@/components/icon/Eth'
+import SolanaIcon from '@/components/icon/SolanaIcon'
+import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
+import { CopyIcon, DollarSign, Text } from 'lucide-react'
+import React, { useEffect } from 'react'
+
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from '@/components/ui/textarea'
+import { useRecoilValue } from 'recoil'
+import { accountAtom } from '@paybox/recoil'
+
+export const Tab = ({
+    chain,
+}: {
+    chain: "Solana" | "Ethereum" | undefined,
+}) => {
+    const account = useRecoilValue(accountAtom);
+    const [tabData, setTabData] = React.useState<{
+        publicKey: string | undefined,
+        name: string | undefined
+    }>()
+
+    useEffect(() => {
+        if (account) {
+            if (chain === "Ethereum") {
+                setTabData({
+                    name: account?.name,
+                    publicKey: account?.eth.publicKey
+                });
+            } else {
+                setTabData({
+                    name: account?.name,
+                    publicKey: account?.sol.publicKey
+                });
+            }
+        }
+    }, [account, chain]);
+
+    const [copyText, setCopyText] = React.useState<string>("Copy")
+    return (
+        <>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <div className="flex gap-x-8 items-center ">
+                        <Button variant={"ghost"} className="flex flex-row items-center justify-start h-16 gap-x-8 w-full border">
+                            <div className="w-10 flex items-center justify-center">
+                                {chain === "Solana" ? <SolanaIcon className="w-10" /> : <EthIcon className="w-6" />}
+                            </div>
+                            <div className="">
+                                <div className="text-base h-1/2 font-semibold w-full text-start">{chain === "Solana" ? "Solana Devnet" : "Ethereum Sepolia"}</div>
+                                {tabData ? <div className="w-full h-1/2 text-start font-normal">
+                                    {tabData.publicKey?.slice(0, 4)}...{tabData.publicKey?.slice(tabData.publicKey?.length - 4)}
+                                </div> :
+                                    <Skeleton className="h-[20px] w-[100px]" />
+                                }
+                            </div>
+                            <div className="flex-grow" />
+                            <div className="qrcode">
+
+                            </div>
+                            <div className="copy">
+                                <Button
+                                    type="submit"
+                                    size="sm"
+                                    className="px-3 flex gap-x-2 font-semibold"
+                                    disabled={!tabData}
+                                    onClick={() => {
+                                        navigator.clipboard.writeText(tabData?.publicKey as string)
+                                        setCopyText("Copied!")
+                                        setTimeout(() => {
+                                            setCopyText("Copy")
+                                        }, 5000)
+                                    }}
+                                >
+                                    <CopyIcon className="h-4 w-4" />
+                                    <div className="">
+                                        {copyText}
+                                    </div>
+                                </Button>
+                            </div>
+                        </Button>
+                    </div>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-sm">
+                    <DialogHeader>
+                        <DialogTitle>{tabData?.name} {chain} Public Key</DialogTitle>
+                        <DialogDescription>
+                            Share this public key to receive money.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="flex items-center space-x-2">
+                        <div className="grid flex-1 gap-2">
+                            <Label htmlFor="link" className="sr-only">
+                                Public Key
+                            </Label>
+                            <Textarea
+                                id="link"
+                                defaultValue={tabData?.publicKey}
+                                placeholder='Public Key'
+                                readOnly
+                                className="min-w-fit resize-none"
+                            />
+                        </div>
+                    </div>
+                    <DialogFooter className="sm:justify-between">
+                        <DialogClose asChild>
+                            <Button type="button" variant="secondary">
+                                Close
+                            </Button>
+                        </DialogClose>
+                        <Button
+                            type="submit"
+                            size="sm"
+                            className="px-3 flex gap-x-2 font-semibold"
+                            disabled={!tabData}
+                            onClick={() => {
+                                navigator.clipboard.writeText(tabData?.publicKey as string)
+                                setCopyText("Copied!")
+                                setTimeout(() => {
+                                    setCopyText("Copy")
+                                }, 5000)
+                            }}
+                        >
+                            <CopyIcon className="h-4 w-4" />
+                            <div className="">
+                                {copyText}
+                            </div>
+                        </Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+
+        </>
+    )
+}
