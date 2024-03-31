f6094d70cbdd9fdb4bbb13f53ed8a9554724a643
Akash Shaw <shawakash2003@gmail.com>
2024-03-31 21:49:35 +0530

    chore: adding top-nav bar

--- apps/web/app/RootChildLayout.tsx
+++ apps/web/app/RootChildLayout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Nav } from "@/components/ui/nav";
 import { Progress } from "@/components/ui/progress";
+import { SiteHeader } from "@/components/ui/siteHeader";
 import { Toaster } from "@/components/ui/sonner";
 import { loadingAtom } from "@paybox/recoil";
 import { useRecoilValue } from "recoil";
@@ -14,7 +15,7 @@ export default function RootChildLayout({
   return (
     <>
       {/* <Progress value={33} className="absolute top-0 bg-transparent" /> */}
-      {/* <Nav /> */}
+      <SiteHeader />
       <main className="flex min-h-screen flex-col w-full justify-center">
           {children}
       </main>
