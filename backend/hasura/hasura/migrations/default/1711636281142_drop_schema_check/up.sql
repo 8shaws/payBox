5c6c17fadbce34c7ef1748c75c00e9153a6188bf
Akash Shaw <shawakash2003@gmail.com>
2024-03-31 17:24:49 +0530

    fix: local image migration apply issue

--- backend/hasura/hasura/migrations/default/1711636281142_drop_schema_check/up.sql
+++
@@ -1,6 +0,0 @@
-DO $$ 
-BEGIN
-    IF EXISTS (SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'check') THEN
-        EXECUTE 'DROP SCHEMA check CASCADE';
-    END IF;
-END $$;
