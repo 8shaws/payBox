e556b78dde2a0f4fc20265306fe17f7aa74e32d2
Akash Shaw <shawakash2003@gmail.com>
2024-04-05 21:48:56 +0530

    chore: test update

--- contract/transfers/tests/transfers.ts
+++ contract/transfers/tests/transfers.ts
@@ -1,5 +1,5 @@
 import * as anchor from '@project-serum/anchor';
-import { Keypair, SystemProgram } from '@solana/web3.js';
+import { Keypair, PublicKey, SystemProgram } from '@solana/web3.js';
 import { decode } from 'bs58';
 import fs from 'fs';
 import dotenv from 'dotenv';
@@ -11,39 +11,41 @@ const idl = JSON.parse(fs.readFileSync('./target/idl/paybox_txn.json', 'utf8'));
 const programId = new anchor.web3.PublicKey(process.env.PROGRAM_ID as string);
 const provider = anchor.AnchorProvider.env();
 anchor.setProvider(provider);
-
+// const program = anchor.workspace.PayboxTxn,
 const program = new anchor.Program(idl, programId, provider);
-const secretKey = new Uint8Array([70, 121, 207, 138, 198, 193, 135, 158, 87, 4, 134, 81, 28, 64, 220, 152, 164, 102, 10, 74, 16, 132, 196, 146, 92, 104, 38, 193, 179, 127, 190, 252, 224, 217, 254, 224, 146, 207, 210, 224, 84, 53, 124, 87, 214, 144, 0, 211, 234, 243, 202, 32, 197, 46, 128, 202, 89, 251, 71, 34, 254, 8, 250, 186]);
-const from = Keypair.fromSecretKey(secretKey);
 
 
 async function main() {
-
-  const client = anchor.web3.Keypair.generate();
+  // let seed = anchor.utils.bytes.utf8.encode(`paybox`);
+  let clientSeed = anchor.utils.bytes.utf8.encode(`account`);
+  let clientPubkey: PublicKey;
+  [clientPubkey] = anchor.web3.PublicKey.findProgramAddressSync(
+    [clientSeed],
+    program.programId
+  );
   // await provider.connection.requestAirdrop(client.publicKey, 10000000);
   const tx = await program.methods
     .addAccount(programId)
     .accounts({
-      client: client.publicKey,
-      signer: provider.wallet.publicKey,
+      client: clientPubkey,
+      authority: provider.wallet.publicKey,
       systemProgram: SystemProgram.programId,
     })
-    .signers([client])
     .rpc();
 
   console.log('Transaction', tx)
   await provider.connection.confirmTransaction(tx, 'processed');
   console.log('Success', tx)
-  // const account = await program.account.client.fetch(client.publicKey);
+  // const account = await program.account.client.fetch(clientPubkey);
   // console.log('Account', account);
-  
   const getLenTx = await program.methods
-  .getLength()
-  .accounts({
-    client: client.publicKey
-  })
-  .rpc();
-  
+    .getLength()
+    .accounts({
+      client: clientPubkey,
+      authority: provider.wallet.publicKey,
+    })
+    .rpc();
+
   await provider.connection.confirmTransaction(getLenTx, 'processed');
   console.log('Transaction', getLenTx)
 }
