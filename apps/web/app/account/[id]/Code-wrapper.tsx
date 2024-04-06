ace3e8de7e7a9a7f53d355a8d1b18af1a48b3b40
Akash Shaw <shawakash2003@gmail.com>
2024-04-06 12:39:15 +0530

    chore: adding account qrcode

---
+++ apps/web/app/account/[id]/Code-wrapper.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { CLIENT_URL } from '@paybox/common';
+import React from 'react'
+import { QRCode } from 'react-qrcode-logo';
+
+export function CodeWrapper({
+    accountId
+}: {
+    accountId: string
+}) {
+    return (
+        <>
+            <QRCode
+                logoImage={`/network/ethDark.png`}
+                logoPadding={5}
+                size={140}
+                logoPaddingStyle="circle"
+                style={{ margin: "auto", padding: "1rem", borderRadius: "10px" }}
+                qrStyle="squares"
+                eyeRadius={10}
+                enableCORS={true}
+                // fgColor="#4287f5"
+                ecLevel="Q"
+                removeQrCodeBehindLogo={true}
+                value={`${CLIENT_URL}/txn/accept?to=${accountId}`}
+            />
+        </>
+    )
+}
--- apps/web/app/account/[id]/page.tsx
+++ apps/web/app/account/[id]/page.tsx
@@ -11,6 +11,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 import React from "react";
 import { Tab } from "./tab";
+import { CodeWrapper } from "./Code-wrapper";
 
 interface PageProps {
     params: { id: string }
@@ -22,28 +23,40 @@ export default async function Page({
 
     return (
         <>
-        <div className="flex h-screen justify-center items-center p-5">
-            <Card className="w-[500px] "> 
-                <CardHeader>
-                    <CardTitle className="w-[100px] h-[22px] text-start items-center flex">{"Account Name" ? "Account" : <Skeleton className="w-[100px] h-[22px]" />} </CardTitle>
-                    <CardDescription>Multi chain Web3 Account by Paybox</CardDescription>
-                </CardHeader>
-                <CardContent className="flex flex-col gap-y-4">
-                    <Tab
-                        chain={"Solana"}
-                        key={"sol"}
-                    />
-                    <Tab
-                        chain={"Ethereum"}
-                        key={"eth"}
-                    />
-                </CardContent>
-                <CardFooter>
-                    <p>Card Footer</p>
-                </CardFooter>
-            </Card>
-        </div>
-            
+            <div className="flex h-screen justify-around items-center p-5">
+                <Card className="w-fit">
+                    <CardHeader>
+                        <CardTitle className="text-start items-center flex">{"Account Name" ? "QRcode" : <Skeleton className="w-[100px] h-[22px]" />} </CardTitle>
+                        <CardDescription>Share this for accepting payment...</CardDescription>
+                    </CardHeader>
+                    <CardContent className="flex flex-col gap-y-4 items-center justify-center">
+                        <CodeWrapper accountId={params.id} />
+                    </CardContent>
+                    <CardFooter>
+                        <p>Use this to accept transactions...</p>
+                    </CardFooter>
+                </Card>
+                <Card className="w-[500px] ">
+                    <CardHeader>
+                        <CardTitle className="w-[100px] h-[22px] text-start items-center flex">{"Account Name" ? "Account" : <Skeleton className="w-[100px] h-[22px]" />} </CardTitle>
+                        <CardDescription>Multi chain Web3 Account by Paybox</CardDescription>
+                    </CardHeader>
+                    <CardContent className="flex flex-col gap-y-4">
+                        <Tab
+                            chain={"Solana"}
+                            key={"sol"}
+                        />
+                        <Tab
+                            chain={"Ethereum"}
+                            key={"eth"}
+                        />
+                    </CardContent>
+                    <CardFooter>
+                        <p>Footer</p>
+                    </CardFooter>
+                </Card>
+            </div>
+
         </>
     );
 }
