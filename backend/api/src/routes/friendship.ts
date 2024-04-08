00345e4e3eb1a41f11ab06efa7701abda77aa727
Akash Shaw <shawakash2003@gmail.com>
2024-04-08 22:11:17 +0530

    chore: added /friendship/pubkey route to get the public keys for friend

---
+++ backend/api/src/routes/friendship.ts
@@ -0,0 +1,36 @@
+import { getFriendPubKey } from "@paybox/backend-common";
+import { GetFriendPubKey, dbResStatus, responseStatus } from "@paybox/common";
+import { Router } from "express";
+
+export const friendshipRouter = Router();
+
+friendshipRouter.get('/pubkey', async (req, res) => {
+    try {
+        //@ts-ignore
+        const id = req.id;
+
+        const { friendId } = GetFriendPubKey.parse(req.query);
+        console.log(friendId)
+        const { status, keys } = await getFriendPubKey(friendId);
+        console.log(keys)
+        if (status === dbResStatus.Error) {
+            return res
+                .status(503)
+                .json({ msg: "Database Error", status: responseStatus.Error });
+        }
+        return res
+            .status(200)
+            .json({
+                status: responseStatus.Ok,
+                keys,
+            });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: responseStatus.Error,
+            msg: "Internal error",
+            error: error,
+        });
+    }
+});
