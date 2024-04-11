e1538229df26995d7666aaf9c40624534170fc09
Akash Shaw <shawakash2003@gmail.com>
2024-04-11 19:42:56 +0530

    feat: added client atom set wrapper maintaining server side rendering

---
+++ apps/web/components/set-client-jwt-wrapper.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { ClientWithJwt } from '@paybox/common';
+import { clientAtom, clientJwtAtom } from '@paybox/recoil';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react'
+import { useSetRecoilState } from 'recoil';
+import { toast } from 'sonner';
+
+function SetClientJwtWrapper({
+  children,
+  client
+}: {
+  children: React.ReactNode;
+  client: ClientWithJwt;
+}) {
+
+    const router = useRouter();
+    const setClientAtom = useSetRecoilState(clientAtom);
+    const setJwtAtom = useSetRecoilState(clientJwtAtom);
+
+    useEffect(() => {
+        if(client) {
+            setClientAtom(client);
+            setJwtAtom(client.jwt);
+        } else {
+            toast('Signup to use the account in Strengths', {
+                action: {
+                    label: 'Onboard',
+                    onClick: () => {
+                        router.push("/signup")
+                    }
+                }
+            });
+        }
+    }, [client])
+
+  return (
+    <>
+        {children}
+    </>
+  )
+}
+
+export default SetClientJwtWrapper
