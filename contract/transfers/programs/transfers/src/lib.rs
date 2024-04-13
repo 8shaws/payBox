7a9ed8cd6a9ded77bbb0f23ba1ddc939c88b7c41
Akash Shaw <shawakash2003@gmail.com>
2024-04-13 15:59:13 +0530

    chore: adding security_txt to program

--- contract/transfers/programs/transfers/src/lib.rs
+++ contract/transfers/programs/transfers/src/lib.rs
@@ -1,11 +1,31 @@
+#![cfg_attr(not(debug_assertions), deny(warnings))]
+
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Token, TokenAccount, Transfer as SplTransfer};
 use anchor_lang::solana_program::system_instruction;
 
-declare_id!("4i1c9jaTU1ZAnfnAVF1DqPeJVaw1vNMY9FYNJP5ALq5t");
+pub mod instructions;
+// use instructions::*;
+
+#[cfg(not(feature = "no-entrypoint"))]
+use solana_security_txt::security_txt;
+
+declare_id!("UdwTHQ9ag5Cvjo1ZMPg3zqTeX1aodFAtVzrnwFGZYhb");
+
+#[cfg(not(feature = "no-entrypoint"))]
+security_txt! {
+    name: "Paybox Wallet Transfer Program",
+    project_url: "https://github.com/shawakash/payBox",
+    contacts: "mailto:dev.paybox@gmail.com",
+    policy: "https://github.com/shawakash/payBox/blob/dev/SECURITY.md",
+    preferred_languages: "en",
+    source_code: "https://github.com/shawakash/payBox",
+    source_release: "v0",
+    auditors: "https://github.com/shawakash/"
+}
 
 #[program]
-pub mod solana_lamport_transfer {
+pub mod paybox {
     use super::*;
 
     pub fn transfer_lamports(ctx: Context<TransferLamports>, amount: u64) -> Result<()> {
