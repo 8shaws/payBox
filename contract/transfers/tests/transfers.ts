6367b0838ef62a472dd9022903a2d256b3140c50
Akash Shaw <shawakash2003@gmail.com>
2024-04-14 19:32:32 +0530

    bug: programId mistmatch

--- contract/transfers/tests/transfers.ts
+++ contract/transfers/tests/transfers.ts
@@ -3,12 +3,13 @@ import { Keypair, PublicKey, SystemProgram } from '@solana/web3.js';
 import { decode } from 'bs58';
 import fs from 'fs';
 import dotenv from 'dotenv';
+import { TOKEN_PROGRAM_ID } from '@project-serum/anchor/dist/cjs/utils/token';
 dotenv.config();
 
 
-const idl = JSON.parse(fs.readFileSync('./target/idl/paybox.json', 'utf8'));
+const idl = JSON.parse(fs.readFileSync('./target/idl/token_contract.json', 'utf8'));
 
-const programId = new anchor.web3.PublicKey("UdwTHQ9ag5Cvjo1ZMPg3zqTeX1aodFAtVzrnwFGZYhb");
+const programId = new anchor.web3.PublicKey("CnQY56UWDHc6kkeENU7jp4QZEkEpUsE3cJNKRm9Q5qvK");
 const provider = anchor.AnchorProvider.env();
 anchor.setProvider(provider);
 // const program = anchor.workspace.PayboxTxn,
@@ -28,15 +29,15 @@ async function main() {
   //   program.programId
   // );
   // await provider.connection.requestAirdrop(from.publicKey, 10000000);
-  const tx = await program.methods
-    .transferLamports(new anchor.BN(1000000))
-    .accounts({
-      from: from.publicKey,
-      to: to.publicKey,
-      systemProgram: SystemProgram.programId,
-    })
-    .signers([from])
-    .rpc();
+  const fromWallet = anchor.web3.Keypair.generate()
+  const mint = anchor.web3.Keypair.generate()
+  const tx = await program.methods.initializeMint().accounts({
+    mint: mint.publicKey,
+    payer: fromWallet.publicKey,
+    systemProgram: anchor.web3.SystemProgram.programId,
+    tokenProgram: program.programId,
+    rent: anchor.web3.SYSVAR_RENT_PUBKEY
+  }).signers([fromWallet, mint]).rpc();
 
   console.log('Transaction', tx)
   await provider.connection.confirmTransaction(tx, 'processed');
