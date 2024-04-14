87867088a2156517c4bf5b84142057abac031201
Akash Shaw <shawakash2003@gmail.com>
2024-04-14 20:28:32 +0530

    chore: mint token func

--- contract/transfers/programs/token_contract/src/lib.rs
+++ contract/transfers/programs/token_contract/src/lib.rs
@@ -1,34 +1,47 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token;
-use anchor_spl::token::{Token, MintTo, Mint, Transfer};
+use anchor_spl::token::{Token, MintTo, Transfer};
 
-declare_id!("CnQY56UWDHc6kkeENU7jp4QZEkEpUsE3cJNKRm9Q5qvK");
 
+declare_id!("2q1PSEpwKHp2SM6wUnEceV8jaLLpmGMxK3mnoRmduX9i");
 
 #[program]
 pub mod token_contract {
     use super::*;
 
-    pub fn initialize_mint(ctx: Context<InitializeMint>) -> Result<()> {
+    pub fn mint_token(ctx: Context<MintToken>, amount: u64) -> Result<()> {
+        // Create the MintTo struct for our context
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+        
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        // Create the CpiContext we need for the request
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        // Execute anchor's helper function to mint tokens
+        token::mint_to(cpi_ctx, amount)?;
+        
         Ok(())
     }
+
+    
 
 }
 
 #[derive(Accounts)]
-pub struct InitializeMint<'info> {
-    #[account(
-        init,
-        payer = payer,
-        mint::decimals = 9,
-        mint::authority = payer,
-        mint::freeze_authority = payer,
-    )]
-    pub mint: Account<'info, Mint>,
+pub struct MintToken<'info> {
+    /// CHECK: This is the token that we want to mint
     #[account(mut)]
-    pub payer: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    pub mint: UncheckedAccount<'info>,
     pub token_program: Program<'info, Token>,
-    ///CHECK: This is not dangerous because we don't read or write from this account
-    pub rent: AccountInfo<'info>,
+    /// CHECK: This is the token account that we want to mint tokens to
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+    /// CHECK: the authority of the mint account
+    #[account(mut)]
+    pub authority: AccountInfo<'info>,
 }
+
