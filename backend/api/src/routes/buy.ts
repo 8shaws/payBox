9ab4920743df2bf1754676d2baec5a3758fc1fc9
Akash Shaw <shawakash2003@gmail.com>
2024-04-16 21:36:45 +0530

    chore(api): adds /quote to get the recent quote for a crypto currency

--- backend/api/src/routes/buy.ts
+++ backend/api/src/routes/buy.ts
@@ -1,10 +1,11 @@
-import { CLIENT_URL, ClientProvider, DBTopics, GetBuyUrlSchema, TopicTypes, TxnStatus, responseStatus } from "@paybox/common";
+import { CLIENT_URL, ClientProvider, DBTopics, GetBuyUrlSchema, GetQuoteSchema, TopicTypes, TxnStatus, responseStatus } from "@paybox/common";
 import { Router, response } from "express";
 import { MoonPay } from '@moonpay/moonpay-node';
 import { MOONPAY_API_KEY, MOONPAY_SECRET_KEY } from "../config";
 import { generateUUID, moonPay } from "..";
 import { NotifWorker } from "../workers/notfi";
 import { providers } from "web3";
+import sdk from '@api/moonpaydocs';
 
 
 export const buyRouter = Router();
@@ -42,7 +43,6 @@ buyRouter.get('/', async (req, res) => {
                         status: responseStatus.Error
                     });
             }
-            console.log(signedUrl);
 
             return res.status(200).json({
                 status: responseStatus.Ok,
@@ -61,5 +61,34 @@ buyRouter.get('/', async (req, res) => {
             error,
             status: responseStatus.Error
         });
+    }
+});
+
+buyRouter.get('/quote', async (req, res) => {
+    try {
+        //@ts-ignore
+        const id = req.id;
+        if (id) {
+
+            const { baseCurrencyAmount, baseCurrencyCode, currencyCode, quoteCurrencyAmount, areFeesIncluded }
+                = GetQuoteSchema.parse(req.body);
+            sdk.auth(MOONPAY_API_KEY);
+            const data = await sdk.getBuyQuote({ currencyCode, query: { baseCurrencyCode, quoteCurrencyAmount, areFeesIncluded, baseCurrencyAmount  } });
+            return res.status(200).json({
+                status: responseStatus.Ok,
+                data
+            });
+        }
+        return res.status(401).json({
+            message: "Auth Error",
+            status: responseStatus.Error,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: "Internal Serve Error",
+            error,
+            status: responseStatus.Error
+        })
     }
 });
