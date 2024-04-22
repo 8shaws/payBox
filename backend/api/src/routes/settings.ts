f15100e6c4b455ed765ce9dd88ae0088e5b2d00f
Akash Shaw <shawakash2003@gmail.com>
2024-04-22 20:35:58 +0530

    chore: adds /get settings

--- backend/api/src/routes/settings.ts
+++ backend/api/src/routes/settings.ts
@@ -1,6 +1,7 @@
-import { TestModeSetSchema, dbResStatus, responseStatus } from "@paybox/common";
+import { Settings, TestModeSetSchema, dbResStatus, responseStatus } from "@paybox/common";
 import { Router } from "express";
-import { updateTestmode } from "../db/settings";
+import { getSettings, updateTestmode } from "../db/settings";
+import { Redis } from "..";
 
 export const settingsRouter = Router();
 
@@ -9,7 +10,10 @@ settingsRouter.post('/testmode', async (req, res) => {
         //@ts-ignore
         const id = req.id;
         if(id) {
-            const {testMode} = TestModeSetSchema.parse(req.query);
+
+            await Redis.getRedisInst().deleteHash(`${id}:settings`)
+
+            const {testMode} = TestModeSetSchema.parse(req.body);
 
             const {status} = await updateTestmode(testMode, id);
             if(status == dbResStatus.Error) {
@@ -32,3 +36,44 @@ settingsRouter.post('/testmode', async (req, res) => {
         })
     }
 });
+
+settingsRouter.get('/', async (req, res) => {
+    try {
+        //@ts-ignore
+        const id = req.id;
+        if(id) {
+            const isCached = await Redis.getRedisInst().settings.getSettingsCache<Settings>(`${id}:settings`);
+            if(isCached && isCached.preferedExplorer) {
+                return res.status(200).json({
+                    status: responseStatus.Ok,
+                    settings: isCached
+                });
+            }
+            const {status, settings} = await getSettings(id);
+            console.log(settings)
+            if(status == dbResStatus.Error || !settings) {
+                return res.status(404).json({
+                    message: 'Could not get settings',
+                    status: responseStatus.Error
+                });
+            }
+
+            await Redis.getRedisInst().settings.cacheSettings(`${id}:settings`, settings, 60*60*24);
+
+            return res.status(200).json({
+                status: responseStatus.Ok,
+                settings
+            });
+        }
+        return res.status(401).json({
+            status: responseStatus.Error,
+            msg: "Auth Error"
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: responseStatus.Error,
+            msg: "Internal Server Error"
+        })
+    }
+})
