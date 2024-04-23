354251ac3e9dcca1c5d66fc7714d536659757b7f
Akash Shaw <shawakash2003@gmail.com>
2024-04-23 20:09:01 +0530

    chore: adds client network atoms

--- packages/recoil/src/atoms/settings.tsx
+++ packages/recoil/src/atoms/settings.tsx
@@ -1,6 +1,25 @@
-import { atom } from "recoil";
+import { BitcoinCluster, EthCluster, SolCluster } from "@paybox/common";
+import { atom, atomFamily } from "recoil";
 
 export const testmodeAtom = atom<boolean>({
     key: "testmodeAtom",
     default: false
 });
+
+export const ethNetAtom = atom<EthCluster>({
+    key: "ethNetAtom",
+    default: EthCluster.Sepolia
+});
+
+export const btcNetAtom = atom<BitcoinCluster>({
+    key: "btcNetAtom",
+    default: BitcoinCluster.Testnet
+});
+
+export const solNetAtom = atom<SolCluster>({
+    key: "solNetAtom",
+    default: SolCluster.Devnet
+});
+
+
+
---
+++ packages/recoil/src/selectors/settings.tsx
@@ -0,0 +1,19 @@
+import { Network } from "@paybox/common";
+import { selector, selectorFamily } from "recoil";
+import { btcNetAtom, ethNetAtom, solNetAtom } from "../atoms";
+
+export const chainNetState = selectorFamily({
+    key: "chainNetState",
+    get: (chain: Network) => ({get}) => {
+        switch(chain) {
+            case Network.Sol:
+                return get(solNetAtom);
+            
+            case Network.Bitcoin:
+                return get(btcNetAtom);
+
+            case Network.Eth:
+                return get(ethNetAtom);
+        }
+    }
+})
