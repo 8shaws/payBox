b1fed46ba463d7db705551fd53c32e76a16ab024
Akash Shaw <shawakash2003@gmail.com>
2024-04-24 22:15:45 +0530

    chore: adds server actions to app

---
+++ apps/web/src/actions/icon.ts
@@ -0,0 +1,28 @@
+import { Network } from "@paybox/common";
+import { BitcoinIcon } from "../components/icon/bitcoin";
+import EthIcon from "../components/icon/Eth";
+import SolanaIcon from "../components/icon/SolanaIcon";
+import { UsdcIcon } from "../components/icon/usdc";
+
+export const getIcon = (net: Network): {
+    icon: React.ElementType
+} => {
+    switch (net) {
+        case Network.Bitcoin:
+            return {
+                icon: BitcoinIcon
+            };
+        case Network.Eth:
+            return {
+                icon: EthIcon
+            };
+        case Network.Sol:
+            return {
+                icon: SolanaIcon
+            };
+        default:
+            return {
+                icon: UsdcIcon
+            };
+    }
+}
