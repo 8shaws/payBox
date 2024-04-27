38c8f1e8c2841e573e609245769dc8d56cdb737a
Akash Shaw <shawakash2003@gmail.com>
2024-04-27 21:43:04 +0530

    fix: docker imgae build types fix

--- packages/recoil/src/atoms/wallet.tsx
+++ packages/recoil/src/atoms/wallet.tsx
@@ -1,12 +1,13 @@
 "use client"
+import { WalletType } from "@paybox/common";
 import { atom } from "recoil";
 
-export const walletAtom = atom({
+export const walletAtom = atom<WalletType | null>({
     default: null,
     key: "walletAtom",
 });
 
-export const walletsAtom = atom({
+export const walletsAtom = atom<WalletType[]>({
     default: [],
     key: "walletsAtom",
 });
