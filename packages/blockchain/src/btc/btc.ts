d5fdb2b2b5b4711fefc7ea5018626712151af66d
Akash Shaw <shawakash2003@gmail.com>
2024-04-29 20:50:36 +0530

    chore: adds btc send txn method

--- packages/blockchain/src/btc/btc.ts
+++ packages/blockchain/src/btc/btc.ts
@@ -3,9 +3,10 @@ import ECPairFactory from 'ecpair';
 import ecc from '@bitcoinerlab/secp256k1';
 import { regtestUtils } from "./_regtestUtils";
 import { BitcoinCluster, WalletKeys } from "@paybox/common";
+import { createPayment, getInputData } from "./utils";
 
 const ECPair = ECPairFactory(ecc);
-// const dhttp = regtestUtils.dhttp;
+const dhttp = regtestUtils.dhttp;
 
 export class Bitcoin {
     private static instance: Bitcoin;
@@ -24,22 +25,54 @@ export class Bitcoin {
         const keyPair = ECPair.makeRandom();
         const privateKey: string = keyPair.toWIF()
 
-        const { address } = bitcoin.payments.p2sh({
-            redeem: bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey }),
-        });
+        const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
 
-        if(!address) {
+        if (!address) {
             throw new Error('Error generating address');
         }
-        console.log(address);
-        
+
         // const result = await dhttp({
         //     method: 'GET',
         //     url: 'https://blockchain.info/rawaddr/' + address,
         // });
 
-        // console.log(result);
         return { privateKey, publicKey: address };
     }
+
+    async signTxn(
+        senderPrivate: string,
+        receiverPubKey: string,
+        amount: number,
+        network: bitcoin.networks.Network,
+        isSegwit: boolean = false,
+    ) {
+        const senderKeyPair = ECPair.fromWIF(senderPrivate);
+
+        const p2wsh = createPayment('p2wsh-p2pk', [senderKeyPair]);
+        const inputData = await getInputData(amount, p2wsh.payment, isSegwit, 'p2wsh');
+        const psbt = new bitcoin.Psbt({ network })
+            .addInput(inputData)
+            .addOutput({
+                address: receiverPubKey,
+                value: amount - 1000,
+            })
+            .signInput(0, p2wsh.keys[0]);
+        psbt.finalizeAllInputs();
+
+
+        //broadcast the transaction
+        const tx = psbt.extractTransaction();
+        await regtestUtils.broadcast(tx.toHex());
+
+        // await regtestUtils.verify({
+        //     txId: tx.getId(),
+        //     address: receiverPubKey,
+        //     vout: inputData.index,
+        //     value: amount - 1000,
+        // });
+
+        return tx.getId();
+    }
+
 
 }
