aa6dfba21a3aac8c2ae669537bed5141451b73b3
Akash Shaw <shawakash2003@gmail.com>
2024-05-05 22:27:18 +0530

    chore: adds all account page

---
+++ apps/web/src/app/account/all/page.tsx
@@ -0,0 +1,9 @@
+import { AccountList } from "../components/accounts-list";
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <AccountList />
+    </div>
+  );
+}
