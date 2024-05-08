a93c671871f06da60b4a4983f888462eb93d9d7b
Akash Shaw <shawakash2003@gmail.com>
2024-05-08 18:59:22 +0530

    chore: fixing the twitter link

--- apps/web/src/config/site.ts
+++ apps/web/src/config/site.ts
@@ -1,11 +1,10 @@
 export const siteConfig = {
   name: "Paybox",
   url: "https://ui.paybox.com",
-  // ogImage: "https://ui.shadcn.com/og.jpg",
   description:
     "A Multi-Chain Web3 Wallet designed for business. Accessible. Efficient. Open Source.",
   links: {
-    twitter: "https://twitter.com/paybox",
+    twitter: "https://twitter.com/shawakash2003",
     github: "https://github.com/shawakash/paybox",
   },
 };
